694f7589b3bc0b9a5ea5221a8add8259ab08f17a
camiloromers <48441162+camiloromers@users.noreply.github.com>
2020-04-03 16:55:55 +0200

    Fix args parameters

--- run_economic_dispatch.py
+++ run_economic_dispatch.py
@@ -84,77 +84,84 @@ def main_run_disptach(pypsa_net,
     prod_p = opf_prod.copy()
     # Apply interpolation in case of step_opf_min greater than 5 min
     if params['step_opf_min'] > 5:
-        print ('Interpolating dispatch to have 5 minutes resolution..')
+        print ('\n => Interpolating dispatch to have 5 minutes resolution..')
         prod_p = interpolate_dispatch(prod_p)
     # Add noise to results
     gen_cap = pypsa_net.generators.p_nom
     prod_p_with_noise = add_noise_gen(prod_p, gen_cap, noise_factor=0.001)
 
     end = time.time()
-    # print('OPF Done......')
     print('Total time {} min'.format(round((end - start)/60, 2)))
+    print('OPF Done......')
 
     return prod_p_with_noise
 
-
-# # Vars to set up...
+# In case to launch by the terminal
+# ++  ++  ++  ++  ++  ++  ++  ++  +
+# Vars to set up...
 # PYPSA_CASE = './L2RPN_2020_ieee_118_pypsa_simplified'
-# REF_DATA_DIR = './reference_data'
-# DESTINATION_PATH = './OPF_rel/'
-# POSSIBLE_MODE_OPF = ['day', 'week', 'month']
+REF_DATA_DIR = './reference_data'
+DESTINATION_PATH = './OPF_rel/'
+POSSIBLE_MODE_OPF = ['day', 'week', 'month']
 
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description='Launch the evaluation of the Grid2Op ("Grid To Operate") code.')
-#     parser.add_argument('--grid_path', default=os.path.abspath(PYPSA_CASE), type=str,
-#                         help='PyPSA grid dir')
-#     parser.add_argument('--conso_full_path', default=os.path.join(os.path.abspath(REF_DATA_DIR), 'load_2007.csv.bz2'), type=str,
-#                        help='Specify the consumption or demanda full name')
-#     parser.add_argument('--gen_const', type=str,
-#                         help='Specify full path of gen max and min constraints')
-#     parser.add_argument('--dest_path', default=os.path.abspath(DESTINATION_PATH), type=str,
-#                          help='Specify the destination dir')
-#     parser.add_argument('--mode_opf', default='day', type=str,
-#                         help='Optimization mode, for the opf (possible values are {})'.format(POSSIBLE_MODE_OPF))
-#     parser.add_argument('--rescaled_min', default=5, type=int,
-#                         help='Run the optimizer every "rescaled_min" minutes (default 5)'),
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Launch the evaluation of the Grid2Op ("Grid To Operate") code.')
+    parser.add_argument('--grid_path', default='', type=str,
+                        help='PyPSA grid dir')
+    parser.add_argument('--conso_full_path', default=os.path.join(os.path.abspath(REF_DATA_DIR), 'load_2007.csv.bz2'), type=str,
+                       help='Specify the consumption or demanda full name')
+    parser.add_argument('--gen_const', type=str,
+                        help='Specify full path of gen max and min constraints')
+    parser.add_argument('--dest_path', default=os.path.abspath(DESTINATION_PATH), type=str,
+                         help='Specify the destination dir')
+    parser.add_argument('--mode_opf', default='day', type=str,
+                        help='Optimization mode, for the opf (possible values are {})'.format(POSSIBLE_MODE_OPF))
+    parser.add_argument('--rescaled_min', default=5, type=int,
+                        help='Run the optimizer every "rescaled_min" minutes (default 5)'),
 
-#     args = parser.parse_args()
-#     if not args.mode_opf.lower() in POSSIBLE_MODE_OPF:
-#         raise RuntimeError("Please provide an argument \"mode_opf\" among {}".format(POSSIBLE_MODE_OPF))
-#     rescaled_min = args.rescaled_min
-#     try:
-#         rescaled_min = int(rescaled_min)
-#     except:
-#         RuntimeError("\"rescaled_min\" argument should be convertible to an integer.")
+    args = parser.parse_args()
+    if not args.mode_opf.lower() in POSSIBLE_MODE_OPF:
+        raise RuntimeError("Please provide an argument \"mode_opf\" among {}".format(POSSIBLE_MODE_OPF))
+    rescaled_min = args.rescaled_min
+    try:
+        rescaled_min = int(rescaled_min)
+    except:
+        RuntimeError("\"rescaled_min\" argument should be convertible to an integer.")
 
-#     if not rescaled_min % 5 == 0:
-#         raise RuntimeError("\"rescaled_min\" argument should be multiple of 5 (so 5, or 15 is ok, but not 17 nor 3)")
+    if not  60 % rescaled_min == 0:
+        raise RuntimeError("\"rescaled_min\" argument should be multiple of 5 (so 5, or 15 is ok, but not 17 nor 3)")
 
-#     # **  **  **  **  ** 
-#     # Load the PyPSA grid
-#     net = pypsa.Network(import_name=args.grid_path)
+    # **  **  **  **  ** 
+    # Load the PyPSA grid
+    net = pypsa.Network(import_name=args.grid_path)
 
-#     # Load consumption data without index
-#     print ('Reading input data -> load...')
-#     demand = pd.read_csv(args.conso_full_path)
+    # Load consumption data without index
+    print ('Reading input data -> load...')
+    demand = pd.read_csv(args.conso_full_path)
 #     demand.drop('datetime', axis=1, inplace=True)
 
-#     # Check if gen contraints are given
-#     if args.gen_const:
-#         gen_max_constraints = pd.read_csv(os.path.join(args.gen_const), 'gen_max_pu.csv.bz2')
-#         gen_min_constraints = pd.read_csv(os.path.join(args.gen_const), 'gen_min_pu.csv.bz2')
-#         gen_const.update({'p_max_pu': gen_max_constraints, 'p_min_pu': gen_min_constraints})
+    # Check if gen contraints are given
+    gen_const = {'p_max_pu': None, 'p_min_pu': None}
+    if args.gen_const:
+        gen_max_constraints = pd.read_csv(os.path.join(args.gen_const), 'gen_max_pu.csv.bz2')
+        gen_min_constraints = pd.read_csv(os.path.join(args.gen_const), 'gen_min_pu.csv.bz2')
+        gen_const.update({'p_max_pu': gen_max_constraints, 'p_min_pu': gen_min_constraints})
 
-#     # Define parameters
-#     params.update({'step_opf_min': args.rescaled_min, 'mode_opf': args.mode_opf.lower()})
-#     # Run Economic Dispatch
-#     prod_p_dispatch = main_run_disptach(net, demand, gen_constraints=gen_const, params=params)
+    # Define parameters
+    params = {'step_opf_min': args.rescaled_min, 
+               'mode_opf': args.mode_opf.lower(),
+             }
+    # Run Economic Dispatch
+    prod_p_dispatch = main_run_disptach(net, 
+                                        demand, 
+                                        gen_constraints=gen_const, 
+                                        params=params)
 
-#     destination_path = os.path.abspath(args.dest_path)
-#     if not os.path.exists(destination_path):
-#         os.makedirs(destination_path)
+    destination_path = os.path.abspath(args.dest_path)
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
 
-#     # Save as csv
-#     prod_p_dispatch.to_csv(os.path.join(destination_path, 'prod_p.csv.bz2'), sep=';', float_format='%.2f', index=False)
+    # Save as csv
+    prod_p_dispatch.to_csv(os.path.join(destination_path, 'prod_p.csv.bz2'), sep=';', float_format='%.2f', index=False)
     
-#     print('OPF Done......')
+    print('OPF Done......')
